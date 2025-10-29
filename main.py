a546270ee364312528ff2278ef20f60f8831a177
alexandreyu <zouzous@orange.fr>
2025-10-29 16:30:52 +0100

    B2

--- main.py
+++ main.py
@@ -3,5 +3,35 @@ import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
 import os 
-import PIL 
+import PIL
+
+def creation_image_balle_noir(i,j,r,R):
+    Im=[]
+    for k in range(R):
+        L=[]
+        for l in range (R):
+            if ((i-k)**2+(j-l)**2)<= r**2:
+                L.append( [1,1,1,1])
+            else:
+                L.append([0,0,0,1])
+        Im.append(L)
+    return np.array(Im)
+
+
+def __main__():
+    test = np.ones(20, 20)
+    # S = creation_image_balle_noir(5,6,1,10)
+
+    plt.imshow(test)
+
+    # data = np.zeros((512, 512, 3), dtype=np.uint8) * 255
+    # data[:, :] = [255, 255, 255]
+
+    # plt.imshow(S)
+    plt.show()
+    # S2 = np.array(S,dtype=np.float32)
+
+
+
+
 
