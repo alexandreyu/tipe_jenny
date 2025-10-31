6b77e355d2abf78725a736d9af6ec24ee94d1f9b
Widee <william.hochart@gmail.com>
2025-10-31 18:54:21 +0100

    Com. update

--- com.py
+++ com.py
@@ -1,5 +1,11 @@
-import pyserial
+import serial
 
-def send_to_arduino(data:list, com="COM3"):
-    port = serial.Serial(com, 9600)
+def send_to_arduino(data:str, com="COM3"):
+    port = serial.Serial(port=com, baudrate=9600, timeout=1)
     port.write(data.encode('utf-8'))
+    read = port.read()
+    port.close()
+    return read
+
+read = send_to_arduino(data="TEST:", com="/dev/cu.usbmodem11101")
+print(read)
